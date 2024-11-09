ccf478860dbff23f9bdc85e1758c0359ab87f59b
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2024-11-09 15:32:42 -0800

    added gap

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -15,7 +15,7 @@
 	<h1 class="text-4xl">Past Events</h1>
 </div>
 
-<div class="grid md:grid-cols-2 w-full gap-2">
+<div class="grid md:grid-cols-2 w-full gap-4">
 	{#each events as event}
 		<div
 			class="card card-side border-4 border-slate-800 bg-primary p-6 text-secondary-content shadow-xl"
