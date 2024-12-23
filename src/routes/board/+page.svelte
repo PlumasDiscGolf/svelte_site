315e217777ed73a453b769e96554d0965bd79b42
LabyrinthWarden <labyrinthwarden13@gmail.com>
2024-12-23 08:40:12 -0800

    updated board page

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -12,7 +12,7 @@
 <div class="mt-8 w-full">
 	<h2 class="text-center text-2xl">Current Members</h2>
 
-	<div class="grid md:grid-cols-2 gap-4 w-full gap-2">
+	<div class="grid md:grid-cols-2 md:gap-4 w-full gap-2">
 		{#each activeMembers as member}
 			<div
 				class="card card-side border-4 border-slate-800 bg-primary p-6 text-secondary-content shadow-xl"
